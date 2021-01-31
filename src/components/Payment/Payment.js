86229542bed0e7f838377a0739d4fb3a9ed25d4d
taurusilver7 <prasad20697@gmail.com>
2021-01-31 15:05:33 +0530

    payment process initiated

---
+++ src/components/Payment/Payment.js
@@ -0,0 +1,142 @@
+import React, { useEffect, useState } from "react";
+import "./Payment.css";
+
+import { useStateValue } from "../../context/StateProvider";
+import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
+import { Link, useHistory } from "react-router-dom";
+import { getCartTotal } from "../../context/reducer";
+import axios from "../../context/axios";
+
+import CurrencyFormat from "react-currency-format";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+const Payment = () => {
+  const [{ cart, user }, dispatch] = useStateValue();
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [processing, setProcessing] = useState("");
+  const [succeeded, setSucceeded] = useState(false);
+  const [clientSecret, setClientSecret] = useState(true);
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    // generate the special stripe secret which allows us to charge the customer.
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        // Stripe expects in currency subunits (paises)
+        url: `/payments/create?total=${getCartTotal(cart) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [cart]);
+
+  console.log("THE SECRET IS >>>", clientSecret);
+  console.log("👱", user);
+
+  const handleSubmit = async (event) => {
+    // stripe functionality
+    event.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        // paymentIntent = payment confirmation. a part of the response obtained.
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+
+        history.replace("/orders");
+      });
+  };
+
+  const handleChange = (event) => {
+    // listen  to changes in CardElement & display any errors as card details are entered.
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{cart?.length} items</Link>)
+        </h1>
+
+        {/* payment section - delivery address */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>23 Avenue</p>
+            <p>Delhi, India</p>
+          </div>
+        </div>
+
+        {/* payment section - review items */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items & Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {cart.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                image={item.image}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* payment section - payment method */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            {/* Stripe method goes here */}
+
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getCartTotal(cart)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"₹"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
+              {/* Errors */}
+              {error && <div>{error}</div>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
