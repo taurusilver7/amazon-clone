f00402f14736ff99c851b1e8cb760f2049edc586
taurusilver7 <prasad20697@gmail.com>
2021-01-31 15:02:48 +0530

    user details added at product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,26 +1,46 @@
 import React from "react";
 import "./Product.css";
 
-const Product = ({ title, image, rating, price }) => {
+import { useStateValue } from "../../context/StateProvider";
+
+const Product = ({ id, title, image, rating, price }) => {
+  const [state, dispatch] = useStateValue();
+
+  // the dispatch action adds items to cart. i.e the state.
+  // console.log("This is the cart >>>>", state);
+
+  const addToCart = () => {
+    // dispatch the item into the data layer.
+    dispatch({
+      type: "ADD_TO_CART",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="info">
         <p>{title}</p>
         <p className="product__price">
-          <small>Rs</small>
+          <small>₹</small>
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
-            {Array(rating).fill().map((_,i) => (
-                <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
             ))}
         </div>
       </div>
-      <img
-        src={image}
-        alt={title}
-      />
-      <button>Add to Cart</button>
+      <img src={image} alt={title} />
+      <button onClick={addToCart}>Add to Cart</button>
     </div>
   );
 };
